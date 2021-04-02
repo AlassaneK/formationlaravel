4bd56dceee03ee7f4607dc71be0117f06e7ee3ca
AlassaneK <alassanekangembega@gmail.com>
2021-04-02 12:00:50 +0000

    deactivating seeders

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,11 @@ namespace App\Providers;
 
 //use Illuminate\Contracts\Pagination\Paginator;
 //use Illuminate\Pagination\AbstractPaginator;
+
+use Doctrine\DBAL\Schema\Schema;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Url;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +30,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        //Schema::defaultStringLength(191);
+        Url::forceScheme('https');
         Paginator::useBootstrap();
     
     }
